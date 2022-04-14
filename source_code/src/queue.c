09fd8c1b3b1451502f0d62127a9b103e95f173dc
duyvt6663 <93929554+duyvt6663@users.noreply.github.com>
2022-04-14 12:42:52 +0700

    Add files via upload

---
+++ source_code/src/queue.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+int empty(struct queue_t * q) {
+	return (q->size == 0);
+}
+
+void enqueue(struct queue_t * q, struct pcb_t * proc) {
+	/* TODO: put a new process to queue [q] */	
+    if(q->size < MAX_QUEUE_SIZE)
+        q->proc[q->size++] = proc;
+}
+
+int peek(struct queue_t * q)
+{
+    /* RETURN the first top priority index
+     * given that q->size > 0
+     * */
+    int idx = 0;
+    uint32_t TP = q->proc[0].priority;
+
+    for(int i = 1; i < q->size; ++i)
+        if(q->proc[i].priority > TP)
+        {
+            TP = q->proc[i].priority;
+            idx = i;
+        }
+
+    return idx;
+}
+
+struct pcb_t * dequeue(struct queue_t * q) {
+	/* TODO: return a pcb whose prioprity is the highest
+	 * in the queue [q] and remember to remove it from q
+	 * */
+    if(q->size > 0)
+    {
+        int idx = peek(q);
+        struct pcb_t * temp = q->proc[idx];
+
+        for(int i = idx; i < q->size - 1; ++i)
+            q->proc[i] = q->proc[i + 1];
+
+        q->size--;
+
+        return temp;
+    }
+	return NULL;
+}
+
