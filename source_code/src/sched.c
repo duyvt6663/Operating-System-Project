e31697f0c63bb47b16e36740e4cdf044d97bfd9e
duyvt6663 <93929554+duyvt6663@users.noreply.github.com>
2022-04-20 15:12:28 +0700

    Update  get_proc in sched.c

--- source_code/src/sched.c
+++ source_code/src/sched.c
@@ -24,6 +24,19 @@ struct pcb_t * get_proc(void) {
 	 * [ready_queue] and return the highest priority one.
 	 * Remember to use lock to protect the queue.
 	 * */
+	
+	pthread_mutex_lock(&queue_lock);
+	if(empty(&ready_queue))
+	{
+		/* Deep move from [run_queue] to [ready_queue] */
+		for(int i = 0; i < run_queue.size; ++i)
+			ready_queue.proc[i] = run_queue.proc[i];
+		
+		run_queue.size = 0;
+	}
+	proc = dequeue(&ready_queue);
+	pthread_mutex_unlock(&queue_lock);
+	
 	return proc;
 }
 
