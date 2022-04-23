372ac4282ae9f4dbf2be9e70cfcbcbe11ec195a2
duyvt6663 <93929554+duyvt6663@users.noreply.github.com>
2022-04-23 18:47:21 +0700

    Update Init_mem

--- source_code/src/os.c
+++ source_code/src/os.c
@@ -145,10 +145,13 @@ int main(int argc, char * argv[]) {
 	}
 	struct timer_id_t * ld_event = attach_event();
 	start_timer();
-
+	
+	/* Init memory, for bloody sake */
+	mem_init();
+	
 	/* Init scheduler */
 	init_scheduler();
-
+	
 	/* Run CPU and loader */
 	pthread_create(&ld, NULL, ld_routine, (void*)ld_event);
 	for (i = 0; i < num_cpus; i++) {
