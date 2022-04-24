43ae03c737900a4026ef075c6e39a4cb6b60073c
Khánh Duy <khanhduy@Khanhs-MacBook-Pro.local>
2022-04-24 18:06:18 +0700

    Fix bug

--- source_code/src/mem.c
+++ source_code/src/mem.c
@@ -118,9 +118,9 @@ addr_t alloc_mem(uint32_t size, struct pcb_t * proc) {
 	 * */
 
 	/* Change value of mem_avail */
-	if ( (proc->bp < RAM_SIZE) &&													 // check for valid break pointer
-			 (proc->bp + num_pages * PAGE_SIZE <= RAM_SIZE) && // check if allocated mem exceeds heap size
-			 (num_pages > 0) )                                 // check if num_pages == 0
+	if ( (proc->bp < RAM_SIZE) &&						   // check for valid break pointer
+		 (proc->bp + num_pages * PAGE_SIZE <= RAM_SIZE) && // check if allocated mem exceeds heap size
+		 (num_pages > 0) )                                 // check if num_pages == 0
 	{
 		int i = 0;
 		while((mem_avail < num_pages) && (i < NUM_PAGES))
@@ -160,12 +160,15 @@ addr_t alloc_mem(uint32_t size, struct pcb_t * proc) {
 				if (page_table == NULL)
 				{
 					page_table = (struct page_table_t *)malloc(sizeof(struct page_table_t));
+
 					//first entry in page table
 					page_table->table[0].v_index = second_lv;
 					page_table->table[0].p_index = i;
 					page_table->size++;
+
 					//add entry to seg_table
-					proc->seg_table->table[proc->seg_table->size++].pages = page_table;
+					proc->seg_table->table[proc->seg_table->size].pages = page_table;
+					proc->seg_table->table[proc->seg_table->size].v_index = first_lv;
 				}
 				else
 				{
@@ -215,6 +218,7 @@ int free_mem(addr_t address, struct pcb_t * proc) {
 	if (translate(address, &p_addr, proc)) {
 		pthread_mutex_lock(&mem_lock);
 		p_addr >>= OFFSET_LEN; 
+
 		while(p_addr != -1)
 		{
 			/* Reset seg and page 
